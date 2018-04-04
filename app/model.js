b551fb0c7733469e0b8cdabcc917b3262360282a
Mahmudur Rahman <nahidacm@gmail.com>
2018-04-04 16:11:06 +0600

    Style update. Line item price update.

--- app/model.js
+++ app/model.js
@@ -1,14 +1,14 @@
 define(['knockout','json!app/products.json'], function(ko,products_json) {
 
     var CartLine = function(item, quantity = 1){
-        return {product: item, quantity: ko.observable(quantity)}
+        line_item_price = item.price*quantity
+        return {product: item, quantity: ko.observable(quantity), line_item_price: ko.observable(line_item_price)}
     }
     
     return function ProductsModel(){
         self = this;
 
         self.products = products_json;
-        console.log(self.products )
         self.cart_items = ko.observableArray();
     
         self.addToCart = function(item){
@@ -18,6 +18,7 @@ define(['knockout','json!app/products.json'], function(ko,products_json) {
                 if(element.product.id === item.id){
                     qty = cart_items[index].quantity()
                     cart_items[index].quantity(++qty)
+                    cart_items[index].line_item_price(qty*cart_items[index].product.price)
                     new_in_cart = false
                     return
                 }
